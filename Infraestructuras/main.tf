e11fdca825cc2055ecba3e8fac92f7c8939ec9ec
piarnedo <piarnedo@acciona.com>
2025-03-17 19:09:47 +0100

    Día 1

---
+++ Infraestructuras/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.108.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+# resource "azurerm_resource_group" "rg" {
+#   name     = "learnk8sResourceGroup"
+#   location = "northeurope"
+# }
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "ClusterProyecto"
+  location            = "West Europe"
+  resource_group_name = "piarnedo2"
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "2"
+    vm_size    = "standard_d2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
